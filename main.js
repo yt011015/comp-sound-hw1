7a8434b377966f1e6261ff1e2665a7d95584c77a
Yuya <94564374+yt27828@users.noreply.github.com>
2024-01-29 20:21:04 -0500

    HW1 done

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // Setting global variables
 var audioCtx;
+var analyser;
+var canvas;
+var canvasContext;
 var oscType = 'sine';
 var globalGain;
 const maxOverallGain = 0.8;
@@ -41,6 +44,33 @@ const keyboardFrequencyMap = {
     '85': 987.766602512248223,  //U - B
 }
 
+const keyboardColorMap = {
+    '90': 'red',     // Z - C
+    '83': 'orange',  // S - C#
+    '88': 'yellow',  // X - D
+    '68': 'green',   // D - D#
+    '67': 'blue',    // C - E
+    '86': 'indigo',  // V - F
+    '71': 'violet',  // G - F#
+    '66': 'brown',   // B - G
+    '72': 'pink',    // H - G#
+    '78': 'purple',  // N - A
+    '74': 'cyan',    // J - A#
+    '77': 'teal',    // M - B
+    '81': 'lime',    // Q - C
+    '50': 'magenta', // 2 - C#
+    '87': 'olive',   // W - D
+    '51': 'coral',   // 3 - D#
+    '69': 'silver',  // E - E
+    '82': 'gold',    // R - F
+    '53': 'orchid',  // 5 - F#
+    '84': 'skyblue', // T - G
+    '54': 'tomato',  // 6 - G#
+    '89': 'navy',    // Y - A
+    '55': 'salmon',  // 7 - A#
+    '85': 'steelblue',// U - B
+};
+
 window.addEventListener('keydown', keyDown, false);
 window.addEventListener('keyup', keyUp, false);
 
@@ -53,9 +83,18 @@ function initializeAudioContext() {
     if (!audioCtx) {
         audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     }
+
     globalGain = audioCtx.createGain(); //this will control the volume of all notes
     globalGain.gain.setValueAtTime(0.8, audioCtx.currentTime)
     globalGain.connect(audioCtx.destination);
+
+    analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 2048;
+    // analyser.connect(audioCtx.destination);
+    canvas = document.getElementById('waveformCanvas');
+    canvasContext = canvas.getContext('2d');
+
+    drawWaveform();
 }
 
 function setOscillatorWaveform(waveform) {
@@ -66,6 +105,7 @@ function keyDown(event) {
     const key = (event.detail || event.which).toString();
     if (keyboardFrequencyMap[key] && !activeOscillators[key]) {
       playNote(key);
+      changeBackgroundColor(key);
     }
 }
 
@@ -76,8 +116,12 @@ function keyUp(event) {
         gainNode.gain.cancelScheduledValues(audioCtx.currentTime);
         gainNode.gain.setTargetAtTime(0, audioCtx.currentTime, releaseTime);
         delete activeOscillators[key];
-        // updateGlobalGain();
+        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+        canvasContext.lineWidth = 2;
+        canvasContext.strokeStyle = 'rgb(0, 0, 0)';
+        canvasContext.beginPath();
     }
+    
 }
 
 function playNote(key) {
@@ -89,10 +133,11 @@ function playNote(key) {
     gainNode.gain.exponentialRampToValueAtTime(0.4, audioCtx.currentTime + attackTime); // Attack
     gainNode.gain.setTargetAtTime(0.2, audioCtx.currentTime + attackTime, decayTime); // Decay
     osc.connect(gainNode).connect(globalGain)
+    osc.connect(analyser);
     osc.start();
     activeOscillators[key] = { osc, gainNode };
 
-    updateGlobalGain()   
+    updateGlobalGain();
 }
 
 function updateGlobalGain() {
@@ -102,4 +147,43 @@ function updateGlobalGain() {
     }
     const newGlobalGain = maxOverallGain / Math.max(1, gainSum);
     globalGain.gain.setValueAtTime(newGlobalGain, audioCtx.currentTime);
+}
+
+function changeBackgroundColor(key) {
+    const color = keyboardColorMap[key];
+    if (color) {
+        document.body.style.backgroundColor = color;
+    }
+}
+
+function drawWaveform() {
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    canvasContext.lineWidth = 2;
+    canvasContext.strokeStyle = 'rgb(0, 0, 0)';
+    canvasContext.beginPath();
+
+    const sliceWidth = (canvas.width * 1.0) / bufferLength;
+    let x = 0;
+
+    for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 128.0;
+        const y = (v * canvas.height) / 2;
+
+        if (i === 0) {
+            canvasContext.moveTo(x, y);
+        } else {
+            canvasContext.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+
+    canvasContext.lineTo(canvas.width, canvas.height / 2);
+    canvasContext.stroke();
+
+    requestAnimationFrame(drawWaveform);
 }
